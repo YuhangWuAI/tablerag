5a70bd0fd46d63647d759cf3ab05f4fc01d61b2a
yuhangwuai <yuhangcs.wu@gmail.com>
2024-07-30 12:35:20 +0100

    scripts

--- scripts/augmentation.sh
+++ scripts/augmentation.sh
@@ -7,7 +7,7 @@ augmentation_types=("term_explanations")
 for augmentation_type in "${augmentation_types[@]}"; do
     for task in "${task_name[@]}"; do
         echo ">>>>>>>>>>>>>>>>>>Start with $task $augmentation_type<<<<<<<<<<<<<<<<<<<<<<"
-        python run.py -t $task -r default -a $augmentation_type --save_jsonl --experiment_name table_augmentation --load_local_dataset --whether_cot --azure_blob --table_token_limit_portion 50 --augmentation_token_limit_portion 30
+        python run.py -t $task -r default -a $augmentation_type --save_jsonl --experiment_name table_augmentation --load_local_dataset --azure_blob --table_token_limit_portion 50 --augmentation_token_limit_portion 30
         echo ">>>>>>>>>>>>>>>>>>Done with $task $augmentation_type<<<<<<<<<<<<<<<<<<<<<<<"
     done
 done
